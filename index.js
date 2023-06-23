b0510d89a8cfc860988118079481c15b0399f65f
saksham2004tiwari <136952977+saksham2004tiwari@users.noreply.github.com>
2023-06-23 09:19:04 +0545

    Grade

--- index.js
+++ index.js
@@ -1,83 +1,81 @@
-// // console.log("Additya");
-// // var {variableName}={datatype value}
-// // var a="a";
-// var array = ["Software Engineering", "Smart Data Discovery", "Advance Java","Database" ]
-// var object = {
-// Software_Engineering: 100,
-// Smart_Data_Discovery: 90,
-// Advance_Java: 88,
-// Database: 90
+// // // // console.log("Additya");
+// // // // var {variableName}={datatype value}
+// // // // var a="a";
+// // // var array = ["Software Engineering", "Smart Data Discovery", "Advance Java","Database" ]
+// // // var object = {
+// // // Software_Engineering: 100,
+// // // Smart_Data_Discovery: 90,
+// // // Advance_Java: 88,
+// // // Database: 90
 
-// }
-// // console.log(array)
-// // console.log(object)
-// // console.log(1)
-// // let cv= ["Saksham", "Tiwari", "Ghanalal Tiwari", "",]
-let cv ={
-    firstname: "Saksham",
-    lastname: "Tiwari",
-    fathername: "Ghanalal Tiwari",
-    subjects:{
-        icp:{
-            wrl:{
-                supervisor:"Sandip Gurung"
-            }
-        }
-    },
-    education:{
-        SEE:{
-            schoolName:"DBS",
-            year:2076,
-            grade:3.55 ,
-        },
-        College:{
-            collegeName:"CHHS",
-            year:2079,
-            grade:3.01,
-        },
-        University:{
-            uniName:"CHHS",
-            year:200,
-            grad:3.5 ,
-        }
-    },
-    workExperience: "3 years",
-    address:{
-        Country: "Nepal",
-        State: "Gandaki",
-        District: "KAski"
-    },
-    // skill:[
-    //     teacher:{
-    //         teacher: "Bipin"
-    //     },
-    //     field: "Web Development",
-    //     time: "4 years",
-    //     company: "Able"
-    // ],
-    skill: [
-        {
-         teacher: "Bipin"
+// // // }
+// // // // console.log(array)
+// // // // console.log(object)
+// // // // console.log(1)
+// // // // let cv= ["Saksham", "Tiwari", "Ghanalal Tiwari", "",]
+
+    // workExperience: "3 years",
+    // address:{
+    //     Country: "Nepal",
+    //     State: "Gandaki",
+    //     District: "KAski"
+    // },
+    
+    // skill: [
+    //     {
+    //      teacher: "Bipin"
           
-        },
-        {field: "Web Development",},
-        {time: "4 years",},
-        {company: "Able"}
-      ],
+    //     },
+    //     {field: "Web Development",},
+    //     {time: "4 years",},
+    //     {company: "Able"}
+    //   ],
        
 
-    Langauges:{
-        English:"A+",
-        Nepali:"A",
-        Hindi:"B"
+    // Langauges:{
+    //     English:"A+",
+    //     Nepali:"A",
+    //     Hindi:"B"
         
+    // }
+let cv ={
+    name: "Saksham Tiwari",
+    class:  "class 12",
+    School:"DBS",
+    education:{
+        College:{
+        Science:{
+            gpa:4  ,
+            marks:98,
+        },
+        English:{
+            gpa:3.9,
+            marks:92,
+        },
+        Maths:{
+            gpa:4,
+            marks:100, // 
+        }
     }
+    },
+
 
 
 }
-// console.log(cv)
-console.log(cv.education.SEE);
-console.log( cv.skill[0].teacher);
-console.log(cv.subjects.icp.wrl.supervisor);
-console.log(cv.Langauges)
+console.log(cv.name);
+console.log(cv.class);
+// To print the total marks
+console.log(cv.education.College.Science.marks + cv.education.College.English.marks + cv.education.College.Maths.marks );
+// to print percentage
+console.log(((cv.education.College.Science.marks + cv.education.College.English.marks + cv.education.College.Maths.marks)/300)*100 + "%" )
+//to print the gpa
+console.log((cv.education.College.Science.gpa + cv.education.College.English.gpa + cv.education.College.Maths.gpa)/3 + "gpa")
+// to print the grade
+if (((cv.education.College.Science.marks + cv.education.College.English.marks + cv.education.College.Maths.marks )/3) >= 90){
+    console.log("Grade A+")
+}
+else if (((cv.education.College.Science.marks + cv.education.College.English.marks + cv.education.College.Maths.marks )/3) >= 80 ){
+    console.log("Grade A")
+}
+
 
